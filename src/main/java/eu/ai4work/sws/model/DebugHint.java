42017b306422cc4a8b402a2be368893ad15e8de2
Umesh Sevra <sevra@atb-bremen.de>
2025-02-26 16:02:53 +0100

    deleted un-needed debug hint

--- src/main/java/eu/ai4work/sws/model/DebugHint.java
+++ src/main/java/eu/ai4work/sws/model/DebugHint.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonValue;
 public enum DebugHint {
     UNKNOWN_INPUT("Invalid sliding decision input. Please check the provided parameter and try again"),
     UNEXPECTED_ERROR("An unexpected error occurred. Check the exception message or system logs for more details"),
-    INVALID_FCL_FILE("Failed to parse the FCL file. Please check the file format and syntax");
 
     private final String debugHintMessage;
     DebugHint(String debugHintMessage) {
